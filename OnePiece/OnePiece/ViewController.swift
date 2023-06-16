51b243b36a632430d43fc80358dd8222c220cd70
Choyoungjun <cyj070513@gmail.com>
2023-06-16 17:41:10 +0900

    📑 :: 프로젝트 생성

---
+++ OnePiece/OnePiece/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SnapKit
+import Then
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ OnePiece/Pods/Target Support Files/Pods-OnePiece/Pods-OnePiece-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OnePiece : NSObject
+@end
+@implementation PodsDummy_Pods_OnePiece
+@end
