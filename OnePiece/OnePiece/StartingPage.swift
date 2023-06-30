9f12dded0deac05aaee30f5e07e1ee18210af845
Choyoungjun <cyj070513@gmail.com>
2023-06-30 11:41:03 +0900

    ⚡️ :: startingPage title 추가

--- OnePiece/OnePiece/StartingPage.swift
+++ OnePiece/OnePiece/StartingPage.swift
@@ -9,6 +9,11 @@ class StartingPage: UIViewController {
         $0.font = UIFont(name: "Orbit-Regular", size: 20)
         $0.textColor = UIColor(named: "charcoal")
     }
+    let titleLabel = UILabel().then {
+        $0.text = "One Piece"
+        $0.font = UIFont(name: "Orbit-Regular", size: 36)
+        $0.textColor = .black
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "baseColor")
@@ -21,11 +26,18 @@ class StartingPage: UIViewController {
     func addSubViews() {
         [
             sloganLabel,
+            titleLabel,
         ].forEach({view.addSubview($0)})
     }
     func makeConstraints() {
         sloganLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(198)
+            //디자인 수정되면 다시 수정하기
+            $0.centerX.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(sloganLabel.snp.bottom).offset(10)
+            //디자인 수정되면 다시 수정하기
             $0.centerX.equalToSuperview()
         }
     }
