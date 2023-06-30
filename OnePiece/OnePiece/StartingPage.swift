48dca46723971058d9fef76853e5dad7aba69f87
Choyoungjun <cyj070513@gmail.com>
2023-06-30 11:29:09 +0900

    ⚡️ :: startingPage 슬로건 추가

--- OnePiece/OnePiece/StartingPage.swift
+++ OnePiece/OnePiece/StartingPage.swift
@@ -4,9 +4,30 @@ import Then
 
 class StartingPage: UIViewController {
 
+    let sloganLabel = UILabel().then {
+        $0.text = "단 한장의 사진으로"
+        $0.font = UIFont(name: "Orbit-Regular", size: 20)
+        $0.textColor = UIColor(named: "charcoal")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "baseColor")
+    }
+    override func viewDidLayoutSubviews() {
+        addSubViews()
+        makeConstraints()
+    }
+    
+    func addSubViews() {
+        [
+            sloganLabel,
+        ].forEach({view.addSubview($0)})
+    }
+    func makeConstraints() {
+        sloganLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(198)
+            $0.centerX.equalToSuperview()
+        }
     }
 }
 
