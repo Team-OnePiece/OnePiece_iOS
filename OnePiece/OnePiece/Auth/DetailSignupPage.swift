13f757a86091317e26d9153b8c558ed630e51bc8
Choyoungjun <cyj070513@gmail.com>
2023-07-01 23:53:07 +0900

    ⚡️ :: 세부사항 페이지 배경색 변경

--- OnePiece/OnePiece/Auth/DetailSignupPage.swift
+++ OnePiece/OnePiece/Auth/DetailSignupPage.swift
@@ -11,7 +11,7 @@ class DetailSignupPage: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor(named: "baseColor")
         // Do any additional setup after loading the view.
     }
     
