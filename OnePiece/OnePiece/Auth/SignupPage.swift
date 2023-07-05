b4dc767058627fe30bf8bafa07975303fbc9614f
Choyoungjun <cyj070513@gmail.com>
2023-07-05 10:43:21 +0900

    ⚡️ :: textField 코드 간소화

--- OnePiece/OnePiece/Auth/SignupPage.swift
+++ OnePiece/OnePiece/Auth/SignupPage.swift
@@ -11,47 +11,15 @@ import Then
 
 class SignupPage: UIViewController {
 
-    let idTextField = UITextField().then {
-        $0.attributedPlaceholder = NSAttributedString(string: "아이디", attributes: [
-                    .foregroundColor: UIColor(named: "charcoal"),
-                    .font: UIFont.systemFont(ofSize: 16)
-                ])
-        $0.autocapitalizationType = .none
-        $0.autocorrectionType = .no
-        $0.font = UIFont.systemFont(ofSize: 16)
-        $0.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 17.0, height: 0.0))
-        $0.leftViewMode = .always
-        $0.rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: -17.0, height: 0.0))
-        $0.rightViewMode = .always
-        $0.backgroundColor = .white
-        $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor(named: "darkGreen")?.cgColor
-        $0.layer.cornerRadius = 10
-    }
+    let idTextField = DefaultTextField(placeholder: "아이디")
     let idCheckButton = UIButton(type: .system).then {
         $0.setTitle("중복확인", for: .normal)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 12)
-        $0.setTitleColor(UIColor(named: "charcoal"), for: .normal)
-        $0.backgroundColor = UIColor(named: "pink")
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.backgroundColor = UIColor(named: "darkGreen")
         $0.layer.cornerRadius = 8
     }
-    let passwordTextField = UITextField().then {
-        $0.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [
-            .foregroundColor: UIColor(named: "charcoal"),
-            .font: UIFont.systemFont(ofSize: 16)
-                ])
-        $0.autocapitalizationType = .none
-        $0.autocorrectionType = .no
-        $0.font = UIFont.systemFont(ofSize: 16)
-        $0.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 17.0, height: 0.0))
-        $0.leftViewMode = .always
-        $0.rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: -17.0, height: 0.0))
-        $0.rightViewMode = .always
-        $0.backgroundColor = .white
-        $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor(named: "darkGreen")?.cgColor
-        $0.layer.cornerRadius = 10
-    }
+    let passwordTextField = DefaultTextField(placeholder: "비밀번호")
     let nextPageButton = UIButton(type: .system).then {
         $0.setTitle("다음", for: .normal)
         $0.setTitleColor(UIColor.white, for: .normal)
