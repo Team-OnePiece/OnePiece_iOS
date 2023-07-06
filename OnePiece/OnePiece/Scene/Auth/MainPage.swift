6d3572452d91468395147111206e3adc4b5647d3
Choyoungjun <cyj070513@gmail.com>
2023-07-06 21:03:22 +0900

    ⚡️ :: 피드 작성 버튼 추가 및 customCell 세팅

--- OnePiece/OnePiece/Scene/Auth/MainPage.swift
+++ OnePiece/OnePiece/Scene/Auth/MainPage.swift
@@ -10,7 +10,7 @@ import SnapKit
 import Then
 
 class MainPage: UIViewController {
-
+    
     var cellArr: [String] =  []
     let mainLogo = UIImageView().then {
         $0.image = UIImage(named: "mainLogo")
@@ -23,21 +23,29 @@ class MainPage: UIViewController {
     let tableView = UITableView().then {
         $0.backgroundColor = UIColor(named: "mainColor-3")
     }
+    let feedPlusButton = UIButton(type: .system).then {
+        $0.backgroundColor = UIColor(named: "mainColor-1")
+        $0.layer.borderWidth = 3
+        $0.layer.backgroundColor = UIColor.white.cgColor
+        $0.layer.cornerRadius = 40
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "mainColor-3")
         navigationItem.hidesBackButton = true
+        feedPlusButton.addTarget(self, action: #selector(clickFeedPlus), for: .touchUpInside)
     }
     override func viewDidLayoutSubviews() {
         addSubViews()
         makeConstraints()
     }
-
+    
     func addSubViews() {
         [
             mainLogo,
             mainLabel,
-            tableView
+            tableView,
+            feedPlusButton
         ].forEach({view.addSubview($0)})
     }
     func makeConstraints() {
@@ -55,5 +63,30 @@ class MainPage: UIViewController {
             $0.bottom.equalToSuperview()
             $0.left.right.equalToSuperview().inset(27)
         }
+        feedPlusButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(44)
+            $0.right.equalToSuperview().inset(30)
+            $0.width.height.equalTo(80)
+        }
+    }
+    @objc func clickFeedPlus() {
+        self.navigationController?.pushViewController(FeedContentPage(), animated: true)
+        let backButton = UIBarButtonItem(title: "피드 작성", style: .plain, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem = backButton
+        self.navigationItem.backBarButtonItem?.tintColor = .black
     }
 }
+
+extension MainPage: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.cellId, for: indexPath) as! CustomCell
+//        cell.textLabel?.text = cellArr[indexPath.row]
+        return cell
+    }
+    
+}
+
