a46f8c1d34e6b7f53aedbb42b5ea8c6a80e5274e
Choyoungjun <cyj070513@gmail.com>
2023-07-06 21:02:21 +0900

    📑 :: 피드 작성 뷰 추가

---
+++ OnePiece/OnePiece/Scene/FeedView/FeedContentPage.swift
@@ -0,0 +1,16 @@
+//
+//  FeedContentPage.swift
+//  OnePiece
+//
+//  Created by 조영준 on 2023/07/06.
+//
+
+import UIKit
+
+class FeedContentPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "mainColor-3")
+    }
+}
