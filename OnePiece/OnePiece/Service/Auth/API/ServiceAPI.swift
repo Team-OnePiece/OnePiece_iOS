4739e39e3e81df0d46d0cc83eff8f529fd5bba3c
Choyoungjun <cyj070513@gmail.com>
2023-07-10 23:14:37 +0900

    ⚡️ :: 서버통신코드 수정

--- OnePiece/OnePiece/Service/Auth/API/ServiceAPI.swift
+++ OnePiece/OnePiece/Service/Auth/API/ServiceAPI.swift
@@ -2,7 +2,8 @@ import Foundation
 import Moya
 
 enum ServiceAPI {
-    case signup(id: String, password: String, nickName: String, profile: String, schoolNumber: Int, classNumber: Int, studentNumber: Int)
+    case signup(id: String, password: String)
+    case signup2(nickName: String, profile: String, schoolNumber: Int, classNumber: String, studentNumber: Int)
     case login(id: String, password: String)
 }
 
@@ -14,7 +15,9 @@ extension ServiceAPI: TargetType {
     var path: String {
         switch self {
         case .signup:
-            return "/user"
+            return "/user/signup"
+        case .signup2:
+            return "/user/signup"
         case .login:
             return "/user/login"
         }
@@ -24,6 +27,8 @@ extension ServiceAPI: TargetType {
         switch self {
         case .signup:
             return .post
+        case .signup2:
+            return .post
         case .login:
             return .post
         }
@@ -31,16 +36,20 @@ extension ServiceAPI: TargetType {
     
     var task: Moya.Task {
         switch self {
-        case .signup(let id, let password, let nickName, let profile, let schoolNumber, let classNumber, let studentNumber):
+        case .signup(let id, let password):
             return .requestParameters(
                 parameters: [
                     "userId": id, //(아이디는 1~20자 영문 대 소문자, 숫자 사용하세요)
-                      "userPassword": password, //(비밀번호는 1~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요)
-                      "nickName": nickName, //(닉네임은 특수문자, 숫자를 제외한 2~9자 한글 영문 대 소문자 사용하세요)
-                      "profile": profile,
-                      "schoolNumber": studentNumber,  // (범위 1 ~ 3)
-                      "classNumber": classNumber,   // (범위 1 ~ 4)
-                      "studentNumber": studentNumber  // (범위 1 ~ 18)
+                    "userPassword": password
+                ], encoding: JSONEncoding.default)
+        case .signup2(let nickName, let profile, let schoolNumber, let classNumber, let studentNumber):
+            return .requestParameters(
+                parameters: [
+                    "nickName": nickName,
+                    "profile": profile,
+                    "schoolNumber": studentNumber,  // (범위 1 ~ 3)
+                    "classNumber": classNumber,   // (범위 1 ~ 4)
+                    "studentNumber": studentNumber
                 ], encoding: JSONEncoding.default)
         case .login(let id, let password):
             return .requestParameters(
