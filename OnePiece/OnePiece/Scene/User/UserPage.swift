b2d85d2895144e154cd24d9a221a365a2d2d754f
Choyoungjun <cyj070513@gmail.com>
2023-07-13 17:15:54 +0900

    ♻️ :: 코드 정리 및 파일명 변경

--- OnePiece/OnePiece/Scene/User/UserPage.swift
+++ OnePiece/OnePiece/Scene/User/UserPage.swift
@@ -9,24 +9,28 @@ import UIKit
 import SnapKit
 import Then
 
-class MyPage: UIViewController {
+class UserPage: UIViewController {
 
     let profile = UIImageView().then {
         $0.image = UIImage(named: "profile")
-        $0.backgroundColor = .red
+        $0.backgroundColor = .white
         $0.layer.cornerRadius = 50
         $0.clipsToBounds = true
     }
-    var userNdame = "user"
     let messageLabel = UILabel().then {
         $0.text = "\t\t\t00님,\n\t\t\t오늘은\n어떤 하루를 보냈나요?"
         $0.numberOfLines = 3
         $0.font = UIFont(name: "Orbit-Regular", size: 20)
         $0.textColor = UIColor(named: "gray-800")
     }
+    let modifyButton = DefaultButton(title: "수정하기", backgroundColor: .white, titleColor: UIColor(named: "gray-700")!).then {
+        $0.layer.borderWidth = 0.5
+        $0.layer.borderColor = UIColor(named: "gray-400")?.cgColor
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        modifyButton.addTarget(self, action: #selector(clickModifyPage), for: .touchUpInside)
     }
     override func viewDidLayoutSubviews() {
         layout()
@@ -34,7 +38,8 @@ class MyPage: UIViewController {
     func layout() {
         [
             profile,
-            messageLabel
+            messageLabel,
+            modifyButton
         ].forEach({view.addSubview($0)})
         
         profile.snp.makeConstraints {
@@ -46,14 +51,16 @@ class MyPage: UIViewController {
             $0.top.equalTo(profile.snp.bottom).offset(45)
             $0.centerX.equalToSuperview()
         }
-        func navigationBar() {
-            let signupBackbutton = UIBarButtonItem(title: "마이페이지", style: .plain, target: nil, action: nil)
-            self.navigationItem.backBarButtonItem = signupBackbutton
-            self.navigationItem.backBarButtonItem?.tintColor = UIColor(named: "gray-800")
-            
-            let doneButton = UIBarButtonItem(title: "확인", style: .plain, target: nil, action: nil)
-            self.navigationItem.rightBarButtonItem = doneButton
-            
+        modifyButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(61)
+            $0.left.right.equalToSuperview().inset(25)
         }
+    }
+
+    @objc func clickModifyPage() {
+        self.navigationController?.pushViewController(UserModifyPage(), animated: true)
+        let myPageBackbutton = UIBarButtonItem(title: "마이페이지", style: .plain, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem = myPageBackbutton
+        self.navigationItem.backBarButtonItem?.tintColor = UIColor(named: "gray-800")
     }
 }
