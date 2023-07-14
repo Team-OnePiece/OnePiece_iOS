ef13a84179a03e145289bd24d1f0c2a133b764d2
Choyoungjun <cyj070513@gmail.com>
2023-07-14 09:02:37 +0900

    ⚡️ :: 프로필 이미지 수정기능 추가

--- OnePiece/OnePiece/Scene/User/UserModifyPage.swift
+++ OnePiece/OnePiece/Scene/User/UserModifyPage.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 import Then
 
-class UserModifyPage: UIViewController {
+class UserModifyPage: UIViewController, UINavigationControllerDelegate {
 
     let profileBackground = UIImageView().then {
         $0.backgroundColor = .white
@@ -27,9 +27,17 @@ class UserModifyPage: UIViewController {
     }
     let idModifyTextField = DefaultTextField(placeholder: "핫걸")
     let idCheckButton = DefaultButton(title: "중복확인", backgroundColor: UIColor(named: "mainColor-1")!, titleColor: UIColor(named: "gray-000")!)
+    @objc func clickModifyButton() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        profileModifyButton.addTarget(self, action: #selector(clickModifyButton), for: .touchUpInside)
     }
     override func viewDidLayoutSubviews() {
         layout()
@@ -64,6 +72,23 @@ class UserModifyPage: UIViewController {
             $0.top.equalTo(profileModifyButton.snp.bottom).offset(60)
             $0.left.equalTo(idModifyTextField.snp.right).offset(4)
             $0.right.equalToSuperview().inset(25)
+        }
+    }
+}
+
+extension UserModifyPage: UIImagePickerControllerDelegate {
+//     이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true) {
+        }
+    }
+
+    // 이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true) {
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.profileBackground.image = img
+            self.profile.isHidden = true
         }
     }
 }
