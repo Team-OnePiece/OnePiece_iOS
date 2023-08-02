d745f578deeee2de8585c093257cd4adc4c82fb0
Choyoungjun <cyj070513@gmail.com>
2023-08-02 21:43:47 +0900

    ⚡️ :: 버튼 클릭 이벤트 추가

--- OnePiece/OnePiece/Component/DefaultButton.swift
+++ OnePiece/OnePiece/Component/DefaultButton.swift
@@ -1,22 +1,10 @@
-//
-//  DefaultButton.swift
-//  OnePiece
-//
-//  Created by 조영준 on 2023/07/07.
-//
-
 import UIKit
 import SnapKit
 import Then
 
 class DefaultButton: UIButton {
     
-    init(
-        title: String,
-        backgroundColor : UIColor,
-        titleColor: UIColor
-    ) {
-        super.init(frame: .zero)
+    private func commonSetup(title: String, backgroundColor: UIColor, titleColor: UIColor) {
         self.titleLabel?.font = UIFont(name: "Orbit-Regular", size: 16)
         self.setTitle(title, for: .normal)
         self.backgroundColor = backgroundColor
@@ -25,14 +13,23 @@ class DefaultButton: UIButton {
         self.alpha = 0.8
     }
     
+    convenience init(type: UIButton.ButtonType, title: String, backgroundColor: UIColor, titleColor: UIColor) {
+        self.init(type: type)
+        commonSetup(title: title, backgroundColor: backgroundColor, titleColor: titleColor)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonSetup(title: "", backgroundColor: .blue, titleColor: .white)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     override func layoutSubviews() {
         super.layoutSubviews()
         setup()
     }
-    
     func setup() {
         self.snp.makeConstraints {
             $0.height.equalTo(48)
