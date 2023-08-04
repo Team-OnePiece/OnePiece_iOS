e668ed1b331cff2964578a4628012c4a42b1f956
Choyoungjun <cyj070513@gmail.com>
2023-08-04 19:20:44 +0900

    프젝 살렸다..

---
+++ OnePieceiOS/OnePieceiOS/Scene/Auth/Signup/SchoolInfoViewController.swift
@@ -0,0 +1,152 @@
+import UIKit
+import Moya
+import SnapKit
+import Then
+
+class SchoolInfoSignupViewController: UIViewController, UITextFieldDelegate {
+    
+    private let stackView = UIStackView().then {
+        $0.distribution = .fillEqually
+        $0.axis = .horizontal
+        $0.backgroundColor = .clear
+        $0.spacing = 5
+    }
+    private let gradeTextField = DefaultTextField(placeholder: "학년")
+    private let classTextField = DefaultTextField(placeholder: "반")
+    private let numberTextField = DefaultTextField(placeholder: "번호")
+    private let nextPageButton = DefaultButton(type: .system, title: "다음", backgroundColor: UIColor(named: "mainColor-1")!, titleColor: UIColor(named: "gray-000")!)
+    private let progressImage = UIImageView(image: UIImage(named: "progress3"))
+    private let schoolInfoEnterLabel = UILabel().then {
+        $0.textColor = .red
+        $0.font = UIFont(name: "Orbit-Regular", size: 12)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        numberTextField.returnKeyType = .done
+        gradeTextField.keyboardType = .numberPad
+        classTextField.keyboardType = .numberPad
+        numberTextField.keyboardType = .numberPad
+        gradeTextField.delegate = self
+        classTextField.delegate = self
+        numberTextField.delegate = self
+        gradeTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.allEditingEvents)
+        classTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.allEditingEvents)
+        numberTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.allEditingEvents)
+        nextPageButton.addTarget(self, action: #selector(clickNextPage), for: .touchUpInside)
+        setupKeyboardObservers()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    override func viewWillLayoutSubviews() {
+        addSubViews()
+        makeConstraints()
+    }
+    private func addSubViews() {
+        [
+            progressImage,
+            stackView,
+            schoolInfoEnterLabel,
+            nextPageButton
+        ].forEach({view.addSubview($0)})
+        [gradeTextField, classTextField, numberTextField].forEach({stackView.addArrangedSubview($0)})
+    }
+    private func makeConstraints() {
+        progressImage.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(131)
+            $0.width.equalTo(340)
+            $0.height.equalTo(35)
+        }
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(progressImage.snp.bottom).offset(43)
+            $0.left.right.equalToSuperview().inset(25)
+        }
+        schoolInfoEnterLabel.snp.makeConstraints {
+            $0.top.equalTo(stackView.snp.bottom).offset(8)
+            $0.left.equalToSuperview().inset(28)
+        }
+        nextPageButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(59)
+            $0.left.right.equalToSuperview().inset(25)
+        }
+    }
+    private func setupKeyboardObservers() {
+          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+      }
+      private func removeKeyboardObservers() {
+          NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+          NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+      }
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            UIView.animate(withDuration: 0.3) {
+                self.nextPageButton.transform = CGAffineTransform(translationX: 0, y: -keyboardHeight + 15)
+            }
+        }
+    }
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.nextPageButton.transform = .identity
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        numberTextField.resignFirstResponder()
+        return true
+    }
+}
+extension SchoolInfoSignupViewController {
+    @objc private func clickNextPage() {
+        let userInfo = UserInfo.shared
+        guard let schoolGrade = gradeTextField.text,
+              let schoolClass = classTextField.text,
+              let schoolNumber = numberTextField.text,
+              !(schoolGrade.isEmpty || schoolClass.isEmpty || schoolNumber.isEmpty)
+        else {
+            schoolInfoEnterLabel.text = "다시 확인하세요."
+            return
+        }
+        let provider = MoyaProvider<AuthAPI>(plugins: [MoyaLoggerPlugin()])
+        provider.request(.studentInfo(grade: Int(schoolGrade)!, classNumber: Int(schoolClass)!, number: Int(schoolNumber)!)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200:
+                    userInfo.grade = Int(schoolGrade)
+                    userInfo.classNumber = Int(schoolClass)
+                    userInfo.number = Int(schoolNumber)
+                    self.schoolInfoEnterLabel.text = ""
+                    self.navigationController?.pushViewController(NickNameSignupViewController(), animated: true)
+                    let signupBackbutton = UIBarButtonItem(title: "회원가입", style: .plain, target: nil, action: nil)
+                    self.navigationItem.backBarButtonItem = signupBackbutton
+                    self.navigationItem.backBarButtonItem?.tintColor = UIColor(named: "gray-800")
+                    signupBackbutton.setTitleTextAttributes([
+                        .font: UIFont(name: "Orbit-Regular", size: 16)
+                    ], for: .normal)
+                case 409:
+                    self.schoolInfoEnterLabel.text = "다시 확인하세요."
+                default:
+                    self.schoolInfoEnterLabel.text = "다시 확인하세요."
+                    print(result.statusCode)
+                }
+            case .failure(let err):
+                print("\(err.localizedDescription)")
+            }
+        }
+    }
+    
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        guard let schoolGrade = gradeTextField.text,
+              let schoolClass = classTextField.text,
+              let schoolNumber = numberTextField.text,
+              !(schoolGrade.isEmpty || schoolClass.isEmpty || schoolNumber.isEmpty)
+        else {
+            nextPageButton.alpha = 0.8
+            return
+        }
+        nextPageButton.alpha = 1.0
+    }
+}
---
+++ OnePieceiOS/OnePieceiOS/Scene/Component/DefaultTextField.swift
@@ -0,0 +1,53 @@
+import UIKit
+import SnapKit
+import Then
+
+class DefaultTextField: UITextField {
+
+    private var placeholderText: String = ""
+
+    init(
+        placeholder: String,
+        isSecure: Bool = false
+    ) {
+        super.init(frame: .zero)
+        self.placeholderText = placeholder
+        self.font = UIFont.systemFont(ofSize: 16)
+        self.autocapitalizationType = .none
+        self.autocorrectionType = .no
+        self.isSecureTextEntry = isSecure
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 8
+        let spacerView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        self.leftView = spacerView
+        self.rightView = spacerView
+        self.leftViewMode = .always
+        self.rightViewMode = .always
+        self.layer.borderColor = UIColor(named: "gray-400")?.cgColor
+        self.layer.borderWidth = 0.5
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        makeConstraints()
+        fieldSetting()
+    }
+    private func makeConstraints() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(48)
+        }
+    }
+    private func fieldSetting() {
+        self.attributedPlaceholder = NSAttributedString(
+            string: placeholderText,
+            attributes: [
+                NSAttributedString.Key.foregroundColor: UIColor(named: "gray-600"),
+                NSAttributedString.Key.font: UIFont(name: "Orbit-Regular", size: 16) as Any
+            ]
+        )
+    }
+}
