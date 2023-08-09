107a32c6bb88bd6fa3c2ab39301abd83e8483a00
Choyoungjun <cyj070513@gmail.com>
2023-08-09 23:01:26 +0900

    ♻️ :: 회원가입 API코드 수정

--- OnePieceiOS/OnePieceiOS/Service/ㅕ소/API/AuthAPI.swift
+++ OnePieceiOS/OnePieceiOS/Service/ㅕ소/API/AuthAPI.swift
@@ -47,16 +47,16 @@ extension AuthAPI: TargetType {
     
     var task: Moya.Task {
         switch self {
-        case .signup(_):
+        case .signup:
             return .requestParameters(
                 parameters: [
-                    "account_id": UserInfo.shared, //(아이디는 1~20자 영문 대 소문자, 숫자 사용하세요)
-                    "password": UserInfo.shared,
-                    "password_valid": UserInfo.shared,
-                    "nickname": UserInfo.shared,
-                    "grade": UserInfo.shared,  // (범위 1 ~ 3)
-                    "class_number": UserInfo.shared,   // (범위 1 ~ 4)
-                    "number": UserInfo.shared
+                    "account_id": UserInfo.shared.accountId, //(아이디는 1~20자 영문 대 소문자, 숫자 사용하세요)
+                    "password": UserInfo.shared.password,
+                    "password_valid": UserInfo.shared.passwordValid,
+                    "nickname": UserInfo.shared.nickName,
+                    "grade": UserInfo.shared.grade,  // (범위 1 ~ 3)
+                    "class_number": UserInfo.shared.classNumber,   // (범위 1 ~ 4)
+                    "number": UserInfo.shared.number
                 ], encoding: JSONEncoding.default)
         case .login(let id, let password):
             return .requestParameters(
