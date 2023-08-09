8687d10a98d832dcb989b52439d1064a4b04ee55
Choyoungjun <cyj070513@gmail.com>
2023-08-10 00:45:13 +0900

    ⚡️ :: 피드 삭제 알림 추가

---
+++ OnePieceiOS/OnePieceiOS/Scene/Component/FeedDeleteAlert.swift
@@ -0,0 +1,29 @@
+//
+//  FeedDeleteAlert.swift
+//  OnePieceiOS
+//
+//  Created by 조영준 on 2023/08/09.
+//
+
+import UIKit
+
+class FeedDeleteAlert: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
