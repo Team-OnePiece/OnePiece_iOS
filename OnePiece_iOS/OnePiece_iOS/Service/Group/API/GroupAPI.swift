3bcd79c2e9053d9697fcbf51196e6a636c5bb506
Choyoungjun <cyj070513@gmail.com>
2023-08-14 17:13:53 +0900

    📑 :: 그룹 API 및 Response 모델 작성

---
+++ OnePiece_iOS/OnePiece_iOS/Service/Group/API/GroupAPI.swift
@@ -0,0 +1,8 @@
+//
+//  GroupAPI.swift
+//  OnePiece_iOS
+//
+//  Created by 조영준 on 2023/08/14.
+//
+
+import Foundation
---
+++ OnePiece_iOS/OnePiece_iOS/Service/Group/Response/GroupResponse.swift
@@ -0,0 +1,8 @@
+//
+//  GroupResponse.swift
+//  OnePiece_iOS
+//
+//  Created by 조영준 on 2023/08/14.
+//
+
+import Foundation
