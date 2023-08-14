e29621ba625178ebf81fa174329f8c4e94e2316b
Choyoungjun <cyj070513@gmail.com>
2023-08-14 17:14:21 +0900

    ⚡️ :: 그룹 API 및 Response 작성

--- OnePiece_iOS/OnePiece_iOS/Service/Group/API/GroupAPI.swift
+++ OnePiece_iOS/OnePiece_iOS/Service/Group/API/GroupAPI.swift
@@ -1,8 +1,30 @@
-//
-//  GroupAPI.swift
-//  OnePiece_iOS
-//
-//  Created by 조영준 on 2023/08/14.
-//
-
 import Foundation
+import Moya
+
+enum GroupAPI {
+    case loadGroup
+}
+
+extension GroupAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://localhost:8080")!
+    }
+    
+    var path: String {
+        return "/group"
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return Header.accessToken.header()
+    }
+    
+    
+}
--- OnePiece_iOS/OnePiece_iOS/Service/Group/Response/GroupResponse.swift
+++ OnePiece_iOS/OnePiece_iOS/Service/Group/Response/GroupResponse.swift
@@ -1,8 +1,11 @@
-//
-//  GroupResponse.swift
-//  OnePiece_iOS
-//
-//  Created by 조영준 on 2023/08/14.
-//
-
 import Foundation
+
+struct GroupResponse: Codable {
+    let grade: Int
+    let classnumber: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case grade
+        case classnumber = "class_number"
+    }
+}
